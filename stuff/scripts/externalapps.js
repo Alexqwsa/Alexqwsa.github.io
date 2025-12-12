784b894b70f663a10397755c7f1200e5257860a5
Alexqwsa <134790082+Alexqwsa@users.noreply.github.com>
2025-12-13 00:28:02 +0100

    more

--- stuff/scripts/externalapps.js
+++ stuff/scripts/externalapps.js
@@ -1,9 +1,12 @@
-//spotify "now playing" integration
+/* spotify "now playing" integration (unfinished)
 const spotifyNowplaying = {
     name: "Spotify Now Playing",
     url: "https://api.spotify.com/v1/me/player/currently-playing"
 };
 
-var spotifynowplayingrequest =
-new XMLHttpRequest().open("GET", spotifyNowplaying.url, true);
+// get the funny looking access token for the thingy
+var spotifyAuthrequest = new XMLHttpRequest().open("POST", "https://accounts.spotify.com/api/token", true);
 
+
+var spotifynowplayingrequest = new XMLHttpRequest().open("GET", spotifyNowplaying.url, true);
+spotifynowplayingrequest.setRequestHeader("Authorization", "Bearer insert-access-token-here"); */
